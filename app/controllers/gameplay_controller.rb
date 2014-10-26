635a202589d42901f87301f485b5dc28a63b1016
Buddy Favors Jr <bfavors@purdue.edu>
2014-10-26 19:46:49 -0400

    - getObjectsInCells now pulls from Google Places.

--- app/controllers/gameplay_controller.rb
+++ app/controllers/gameplay_controller.rb
@@ -17,9 +17,9 @@ class GameplayController < ApplicationController
                 1412819501000,
                 {
                     'locationE6' => {
-                        'latE6' => 37.132839 * 1E6,
-                        'lngE6' => -95.785483 * 1E6
-                    },#37.132839,-95.785583
+                        'latE6' => 40.42511 * 1E6,
+                        'lngE6' => -86.914969 * 1E6
+                    },
                     'controllingTeam' => {
                         'team' => 'RESISTANCE'
                     },
@@ -39,62 +39,94 @@ class GameplayController < ApplicationController
   end
 
   def getObjectsInCells
+    location = params['params']['location'];
+    location = location.split(',')
+    location.each_index do |index|
+      location[index] = Integer(location[index]) / 1E6
+    end
+    client = GooglePlaces::Client.new('AIzaSyA9eiiu-ZcQPlRcYiz-XFIt5C2ZfHvaieM')
+    spots = client.spots(location[0], location[1], :radius => 200, :types => [
+        'church',
+        'cemetery',
+        'city_hall',
+        'courthouse',
+        'embassy',
+        'fire_station',
+        'funeral_home',
+        'hindu_temple',
+        'hospital',
+        'library',
+        'museum',
+        'park',
+        'place_of_worship',
+        'police',
+        'post_office',
+        'school',
+        'stadium',
+        'university',
+        'zoo'
+    ])
+    entities = [];
+
+    spots.each do |spot|
+      entity = [
+          # guid
+          spot.id,
+          # timestamp
+          1412819501000,
+          {
+              'locationE6' => {
+                  'latE6' => spot.lat * 1E6,
+                  'lngE6' => spot.lng * 1E6
+              },
+              'resourceWithLevels' => {
+                  'resourceType' => nil
+              },
+              'modResource' => {
+                  'resourceType' => nil
+              },
+              'controllingTeam' => {
+                  'team' => ''
+              },
+              'imageByUrl' => {
+                  'imageUrl' => spot.icon
+              },
+              'portalV2' => {
+                  'descriptiveText' => {
+                      'TITLE' => spot.name,
+                      'ADDRESS' => spot.vicinity
+                  },
+                  'linkedModArray' => [
+                      nil,
+                      nil,
+                      nil,
+                      nil
+                  ]
+              },
+              'captured' => {
+                  'capturingPlayerId' => '40e2a146907e493fa1902705c46eba78',
+                  'nickname' => 'uncaptured'
+              },
+              'resonatorArray' => {
+                  'resonators' => [
+                      nil,
+                      nil,
+                      nil,
+                      nil,
+                      nil,
+                      nil,
+                      nil,
+                      nil
+                  ]
+              }
+          }
+      ]
+      entities.push(entity)
+    end
+
     render json: {
         'gameBasket' => {
-            'gameEntities' => [
-                [
-                    # guid
-                    '1785f1827bde4308afd2cf4aeaadf542',
-                    # timestamp
-                    1412819501000,
-                    {
-                        'locationE6' => {
-                            'latE6' => 40.42511 * 1E6,
-                            'lngE6' => -86.914969 * 1E6
-                        },#40.42511,-86.914969
-                        'resourceWithLevels' => {
-                            'resourceType' => nil
-                        },
-                        'modResource' => {
-                            'resourceType' => nil
-                        },
-                        'controllingTeam' => {
-                            'team' => 'RESISTANCE'
-                        },
-                        'imageByUrl' => {
-                            'imageUrl' => 'http://www.sanjoseca.gov/images/pages/N2326/Station%202.jpg'
-                        },
-                        'portalV2' => {
-                            'descriptiveText' => {
-                                'TITLE' => 'Fire Station',
-                                'ADDRESS' => '123 fake street'
-                            },
-                            'linkedModArray' => [
-                                nil,
-                                nil,
-                                nil,
-                                nil
-                            ]
-                        },
-                        'captured' => {
-                            'capturingPlayerId' => 'bf9e67417ca942cd818577e39db5bdaf',
-                            'nickname' => 'lilpimpollo'
-                        },
-                        'resonatorArray' => {
-                            'resonators' => [
-                                nil,
-                                nil,
-                                nil,
-                                nil,
-                                nil,
-                                nil,
-                                nil,
-                                nil
-                            ]
-                        }
-                    }
-                ]
-            ]
+            'gameEntities' => entities
         }
     }
   end
